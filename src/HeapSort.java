06db9521b4b197e560a7a18f0b3e636908b49fba
Toddthegod1 <143446844+Toddthegod1@users.noreply.github.com>
2024-11-12 11:23:40 -0500

    created minheapsort

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -2,8 +2,42 @@ import java.util.Arrays;
 
 public class HeapSort {
     public static void heapSort(Integer[] values) {
-        // Intentionally not implemented -- see homework assignmnet
-        throw new UnsupportedOperationException("Not yet implemented.");
+        int n = values.length;
+        for (int i = n/2 - 1; i >= 0; i--) {
+            heapify(values,n,i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            swap(values, 0, i);
+
+            heapify(values, i, 0);
+        }
+       
+    }
+
+    private static void heapify(Integer[] array, int heapSize, int rootIndex) {
+        int smallest = rootIndex;
+        int leftChild =  2 * rootIndex + 1;
+        int rightChild = 2 * rootIndex + 2;
+
+        if (leftChild < heapSize && array[leftChild] < array[smallest]) {
+            smallest = leftChild;
+        }
+
+        if (rightChild < heapSize && array[rightChild] < array[smallest]) {
+            smallest = rightChild;
+        }
+        
+        if (smallest != rootIndex) {
+            swap(array, rootIndex, smallest);
+            heapify(array, heapSize, smallest);
+        }
+    }
+
+    private static void swap(Integer[] array, int i, int j) {
+        Integer temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
     public static void main(String[] args) {
